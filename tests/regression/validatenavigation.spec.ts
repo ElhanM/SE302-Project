ec380ef1d964aa6058ff1816c5ad983f1e402da5
Mirza404 <mirzabdulahovic@gmail.com>
2025-01-02 13:55:50 +0100

    Unnecessary wrappers and functions in the POM removed

--- tests/regression/validatenavigation.spec.ts
+++ tests/regression/validatenavigation.spec.ts
@@ -8,8 +8,6 @@ test.describe('My Account Navigation', () => {
   let homePage: HomePage
   let myAccountPage: MyAccountPage
 
-  const baseURL = 'https://ecommerce-playground.lambdatest.io'
-
   test.beforeEach(async ({ page }) => {
     homePage = new HomePage(page)
     myAccountPage = new MyAccountPage(page)
@@ -21,22 +19,27 @@ test.describe('My Account Navigation', () => {
   test.describe('Positive Tests', () => {
     test('should navigate to Register page from My Account', async ({
       page,
+      baseURL,
     }) => {
-      await myAccountPage.clickRegister()
+      await myAccountPage.registerLink.click()
       await expect(page).toHaveURL(
         `${baseURL}/index.php?route=account/register`,
       )
     })
 
-    test('should navigate to Login page from My Account', async ({ page }) => {
-      await myAccountPage.clickLogin()
+    test('should navigate to Login page from My Account', async ({
+      page,
+      baseURL,
+    }) => {
+      await myAccountPage.loginLink.click()
       await expect(page).toHaveURL(`${baseURL}/index.php?route=account/login`)
     })
 
     test('should navigate to Forgotten Password page from My Account', async ({
       page,
+      baseURL,
     }) => {
-      await myAccountPage.clickForgottenPassword()
+      await myAccountPage.forgottenPasswordLink.click()
       await expect(page).toHaveURL(
         `${baseURL}/index.php?route=account/forgotten`,
       )
@@ -47,16 +50,18 @@ test.describe('My Account Navigation', () => {
   test.describe('Negative Tests', () => {
     test('should not access Address Book page without login', async ({
       page,
+      baseURL,
     }) => {
-      await myAccountPage.clickAddressBook()
+      await myAccountPage.addressBookLink.click()
       await expect(page).toHaveURL(`${baseURL}/index.php?route=account/login`)
       console.log('Negative test passed: Redirection to login page occurred')
     })
 
     test('should not access Transactions page from My Account if not logged in', async ({
       page,
+      baseURL,
     }) => {
-      await myAccountPage.clickTransactions()
+      await myAccountPage.transactionsLink.click()
       await expect(page).toHaveURL(`${baseURL}/index.php?route=account/login`)
     })
   })
