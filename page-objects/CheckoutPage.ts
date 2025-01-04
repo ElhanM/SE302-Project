03eb40f9822369da4f3b04a52f4978c4129f2edf
Mustafa Sinanovic <mustafasinanovic@outlook.com>
2025-01-04 10:02:52 +0100

    Fix: Implement negative test case for invalid address during checkout

--- page-objects/CheckoutPage.ts
+++ page-objects/CheckoutPage.ts
@@ -14,31 +14,33 @@ export class CheckoutPage {
   }
 
   // CheckoutPage.ts
-async fillGuestCheckoutDetails(details: {
-  firstName: string,
-  lastName: string,
-  email: string,
-  phone: string,
-  company: string,
-  address1: string,
-  address2: string,
-  city: string,
-  postcode: string,
-  countryValue: string,
-  stateValue: string,
-}) {
-  await this.page.locator('#input-payment-firstname').fill(details.firstName);
-  await this.page.locator('#input-payment-lastname').fill(details.lastName);
-  await this.page.locator('#input-payment-email').fill(details.email);
-  await this.page.locator('#input-payment-telephone').fill(details.phone);
-  await this.page.locator('#input-payment-company').fill(details.company);
-  await this.page.locator('#input-payment-address-1').fill(details.address1);
-  await this.page.locator('#input-payment-address-2').fill(details.address2);
-  await this.page.locator('#input-payment-city').fill(details.city);
-  await this.page.locator('#input-payment-postcode').fill(details.postcode);
-  await this.page.selectOption('#input-payment-country', { value: details.countryValue });
-  await this.page.selectOption('#input-payment-zone', { value: details.stateValue });
-
-}
-
+  async fillGuestCheckoutDetails(details: {
+    firstName: string
+    lastName: string
+    email: string
+    phone: string
+    company: string
+    address1: string
+    address2: string
+    city: string
+    postcode: string
+    countryValue: string
+    stateValue: string
+  }) {
+    await this.page.locator('#input-payment-firstname').fill(details.firstName)
+    await this.page.locator('#input-payment-lastname').fill(details.lastName)
+    await this.page.locator('#input-payment-email').fill(details.email)
+    await this.page.locator('#input-payment-telephone').fill(details.phone)
+    await this.page.locator('#input-payment-company').fill(details.company)
+    await this.page.locator('#input-payment-address-1').fill(details.address1)
+    await this.page.locator('#input-payment-address-2').fill(details.address2)
+    await this.page.locator('#input-payment-city').fill(details.city)
+    await this.page.locator('#input-payment-postcode').fill(details.postcode)
+    await this.page.selectOption('#input-payment-country', {
+      value: details.countryValue,
+    })
+    await this.page.selectOption('#input-payment-zone', {
+      value: details.stateValue,
+    })
+  }
 }
